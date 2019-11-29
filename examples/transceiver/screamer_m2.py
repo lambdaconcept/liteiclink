e1e4800a2af449f592cb66d54477cb5e9988efc0
Pierre-Olivier Vauboin <po@lambdaconcept.com>
2019-11-29 18:53:49 +0100

    examples/screamer_m2: working link on screamer m2

---
+++ examples/transceiver/screamer_m2.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python3
+
+from nmigen import *
+from nmigen.lib.cdc import ResetSynchronizer
+
+from nmigen.build import *
+from nmigen.vendor.xilinx_7series import *
+from nmigen_boards.resources import *
+
+from lib.soc.cores.rs232 import *
+from lib.soc.interconnect import stream
+
+from porting.litex.soc.cores.clock import S7PLL
+from liteiclink.transceiver.gtp_7series import GTPQuadPLL, GTP
+
+class Platform(Xilinx7SeriesPlatform):
+    device      = "xc7a35t"
+    package     = "csg325"
+    speed       = "2L"
+    default_clk = "clk100"
+    resources   = [
+        Resource("clk100", 0, Pins("R2", dir="i"),
+                 Clock(100e6), Attrs(IOSTANDARD="LVCMOS33")),
+
+        *LEDResources(pins="V17 U17", attrs=Attrs(IOSTANDARD="LVCMOS33")),
+
+        UARTResource(0,
+            rx="U4", tx="V4",
+            attrs=Attrs(IOSTANDARD="LVCMOS33")
+        ),
+
+        Resource("pcie_tx", 0, DiffPairs("H2", "H1", dir="o")),
+        Resource("pcie_rx", 0, DiffPairs("E4", "E3", dir="i")),
+    ]
+    connectors  = []
+
+    def toolchain_prepare(self, fragment, name, **kwargs):
+        overrides = {
+            "add_constraints":
+                "set_property SEVERITY {Warning} [get_drc_checks REQP-49]\n"
+                # XXX PO hardcoded below
+                "create_clock -name tx_clk -period 16.0 [get_nets tx_clk]\n"
+                "create_clock -name rx_clk -period 16.0 [get_nets rx_clk]\n"
+                "set_clock_groups -group [get_clocks -include_generated_clocks -of [get_nets crg_clk100_0__i]] -group [get_clocks -include_generated_clocks -of [get_nets tx_clk]] -asynchronous\n"
+                "set_clock_groups -group [get_clocks -include_generated_clocks -of [get_nets crg_clk100_0__i]] -group [get_clocks -include_generated_clocks -of [get_nets rx_clk]] -asynchronous\n"
+                "set_clock_groups -group [get_clocks -include_generated_clocks -of [get_nets tx_clk]] -group [get_clocks -include_generated_clocks -of [get_nets rx_clk]] -asynchronous\n"
+        }
+        return super().toolchain_prepare(fragment, name, **overrides, **kwargs)
+
+
+class _CRG(Elaboratable):
+    def __init__(self, clk, rst):
+        self.clk = clk
+        self.rst = rst
+
+    def elaborate(self, platform):
+        m = Module()
+
+        m.domains.sync = ClockDomain()
+        m.domains.clk125 = cd_clk125 = ClockDomain("clk125")
+
+        # # #
+
+        m.d.comb += ClockSignal().eq(self.clk)
+        m.submodules.reset_sync = ResetSynchronizer(self.rst, domain="sync")
+
+        m.submodules.pll = pll = S7PLL()
+        pll.register_clkin(self.clk, 100e6)
+        pll.create_clkout(cd_clk125, 125e6)
+
+        return m
+
+
+class GTPTestTop(Elaboratable):
+    def elaborate(self, platform):
+        m = Module()
+
+        sys_clk_freq = int(100e6)
+        clk100 = platform.request("clk100")
+        m.submodules += _CRG(clk100.i, Signal())
+
+        # refclk
+        refclk = Signal()
+        m.d.comb += refclk.eq(ClockSignal("clk125"))
+
+        # pll
+        qpll = GTPQuadPLL(refclk, 125e6, 1.25e9)
+        print(qpll)
+        m.submodules += qpll
+
+        # gtp
+        tx_pads = platform.request("pcie_tx", 0, dir="-")
+        rx_pads = platform.request("pcie_rx", 0, dir="-")
+        gtp = GTP(qpll, tx_pads, rx_pads, sys_clk_freq,
+            data_width=20,
+            clock_aligner=False,
+            tx_buffer_enable=True,
+            rx_buffer_enable=True)
+        gtp.add_stream_endpoints()
+        m.submodules += gtp
+
+        # uart
+        uart = RS232PHY(platform.request("uart"), sys_clk_freq, baudrate=115200)
+        m.submodules += uart
+
+        # cdctx
+        cdctx = stream.AsyncFIFO([("data", 8)], 8, r_domain="tx", w_domain="sync")
+        m.submodules += cdctx
+        m.d.comb += uart.source.connect(cdctx.sink)
+
+        # cdcrx
+        cdcrx = stream.AsyncFIFO([("data", 8)], 8, r_domain="sync", w_domain="rx")
+        m.submodules += cdcrx
+        m.d.comb += cdcrx.source.connect(uart.sink)
+
+        # counter
+        counter = Signal(32)
+        m.d.tx += counter.eq(counter + 1)
+
+        # tx path
+        with m.If(cdctx.source.valid):
+            m.d.comb += [
+                cdctx.source.ready.eq(1),
+                gtp.sink.valid.eq(1), # gtp.sink always ready
+                gtp.sink.ctrl.eq(0b00),
+                gtp.sink.data[0:8].eq(0),
+                gtp.sink.data[8:16].eq(cdctx.source.data),
+            ]
+        with m.Else():
+            # K28.5 and slow counter --> TX
+            m.d.comb += [
+                gtp.sink.valid.eq(1),
+                gtp.sink.ctrl.eq(0b01),
+                gtp.sink.data[0:8].eq((5 << 5) | 28),
+                gtp.sink.data[8:16].eq(counter[26:]),
+            ]
+
+        # rx path
+        m.d.rx += [
+            gtp.rx_align.eq(1),
+            gtp.source.ready.eq(1),
+        ]
+        with m.If(gtp.source.ctrl == 0): # gtp.source always valid
+            m.d.rx += [
+                cdcrx.sink.valid.eq(1), # assume fifo ready
+                cdcrx.sink.data.eq(gtp.source.data[8:16]),
+            ]
+        with m.Else():
+            # RX (slow counter) --> Leds
+            m.d.rx += [
+                cdcrx.sink.valid.eq(0),
+                platform.request("led", 0).eq(gtp.source.data[8]),
+                platform.request("led", 1).eq(gtp.source.data[9]),
+            ]
+
+        # self.crg.cd_sync.clk.attr.add("keep")
+        # gtp.cd_tx.clk.attr.add("keep")
+        # gtp.cd_rx.clk.attr.add("keep")
+        # platform.add_period_constraint(gtp.cd_tx.clk, 1e9/gtp.tx_clk_freq)
+        # platform.add_period_constraint(gtp.cd_rx.clk, 1e9/gtp.rx_clk_freq)
+        # platform.add_false_path_constraints(
+        #     self.crg.cd_sync.clk,
+        #     gtp.cd_tx.clk,
+        #     gtp.cd_rx.clk)
+
+        return m
+
+
+def main():
+    platform = Platform()
+    platform.build(GTPTestTop(), name="screamer_m2")
+
+
+if __name__ == "__main__":
+    main()
